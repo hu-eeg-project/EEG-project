2c8aedf1b4cdd08498b415f04aa2c3bd4e733f84
Stefan de Beer <stefan.debeer@student.hu.nl>
2018-10-30 11:18:50 +0100

    Added a wave generator

---
+++ src/pc-parser/src/wave-generator.hh
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <chrono>
+
+#include "array.hh"
+
+typedef struct {
+    double amp;
+    uint16_t freq;
+}Frequency_t;
+
+class WaveGenerator{
+    const Frequency_t * frequencies;
+    size_t size;
+    ArrayPair<RollingArray<double>, RollingArray<double>> * aPair;
+    std::chrono::time_point<std::chrono::high_resolution_clock> st, nt;
+    
+public:
+    WaveGenerator(const Frequency_t * frequencies,
+		  size_t size,
+		  ArrayPair<RollingArray<double>, RollingArray<double>> * aPair);
+    void genSample();
+};
