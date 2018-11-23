97246245e034d3ca554daf1b691f17bc05445978
Stefan de Beer <stefan.debeer@student.hu.nl>
2018-11-23 15:09:18 +0100

    changed uint16_t int16_t

--- src/pc-parser/src/eeg-data-decoding.hh
+++ src/pc-parser/src/eeg-data-decoding.hh
@@ -5,8 +5,8 @@
 #include "eeg-graph.hh"
 
 void loopDecodeNonBatched(SerialInterface& sf,
-                          ArrayPair<RollingArray<uint16_t>, RollingArray<Double_t>>* array);
+                          ArrayPair<RollingArray<int16_t>, RollingArray<Double_t>>* array);
 
 void loopDecodeBatched(SerialInterface& sf,
-                       ArrayPair<RollingArray<uint16_t>, RollingArray<Double_t>>* array,
+                       ArrayPair<RollingArray<int16_t>, RollingArray<Double_t>>* array,
                        const size_t frame_size);
