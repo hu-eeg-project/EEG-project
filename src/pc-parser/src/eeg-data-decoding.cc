0836a133fd7e9a73709da1ef2e160a3b57ca23ad
kasumi <kasumi@kasumi-owari.moe>
2018-11-23 15:37:52 +0100

    Added new data encoding for batches from the ESP.

--- src/pc-parser/src/eeg-data-decoding.cc
+++ src/pc-parser/src/eeg-data-decoding.cc
@@ -2,6 +2,30 @@
 #include <iostream>
 #include <sstream>
 
+#define ERROR_UNKOWN_CMD   0x01
+#define ERROR_BAD_SIGNAL   0x02
+#define ERROR_BAD_CHECKSUM 0x03
+#define ERROR_PACKET_LEN   0x04
+
+std::string getStringForError(int error_code)
+{
+    switch (error_code)
+    {
+        case ERROR_UNKOWN_CMD:
+            return "Unknown CMD";
+        case ERROR_BAD_SIGNAL:
+            return "Bad Signal";
+        case ERROR_BAD_CHECKSUM:
+            return "Bad Checksum";
+        case ERROR_PACKET_LEN:
+            return "Packet Length";
+        default:
+            break;
+    }
+
+    return "Unknown Error Code";
+}
+
 void loopDecodeNonBatched(SerialInterface& sf,
                           ArrayPair<RollingArray<int16_t>, RollingArray<Double_t>>* array)
 {
@@ -28,50 +52,78 @@ void loopDecodeNonBatched(SerialInterface& sf,
 }
 
 void loopDecodeBatched(SerialInterface& sf,
-                       ArrayPair<RollingArray<int16_t>, RollingArray<Double_t>>* array,
+                       ArrayPair<RollingArray<int16_t>,
+                       RollingArray<Double_t>>* array,
                        const size_t frame_size)
 {
-    std::chrono::time_point<std::chrono::high_resolution_clock> st, nt;
-    st = std::chrono::high_resolution_clock::now();
-    
-    size_t frame_index = 0;
-    int16_t * frame = new int16_t[frame_size];
-    int32_t frame_dur = 0;
-    double last = 0;
-    while(true) {
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> st =
+        std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> dt;
+    double frame_start_time;
+
+    int number_of_points;
+    int current_point = 0;
+    int frame_duration;
+    double point_increment;
+
+    while (true)
+    {
         std::string input = sf.getNextLine();
-        uint point_dur;
-        if(input.size()){
-            std::stringstream stream(input);
+
+        if (input.size()) {
+            std::stringstream input_stream(input);
             char type, delim;
-            int16_t eeg_data;
-            stream >> type >> delim >> eeg_data >> delim >> point_dur;
-            if(type == 'd'){
-                frame[frame_index] = eeg_data;
-                frame_index+=1;
-                frame_dur += point_dur;
-                if(frame_index >= frame_size){
-                    nt = std::chrono::high_resolution_clock::now();
-                    std::chrono::duration<double> dt = nt-st;
-                    double end_time = dt.count() + frame_dur;
-                    std::cout<<frame_dur<<std::endl;
-                    double avrg_frame_time = (double)frame_dur / frame_size / 1000000;
+
+            input_stream >> type >> delim;
+
+            switch (type)
+            {
+                case 'f':
+                    input_stream >> number_of_points >> delim >> frame_duration;
+
+                    dt = std::chrono::high_resolution_clock::now() - st;
+                    frame_start_time = dt.count() - frame_duration / 1000000;
+                    point_increment = frame_duration / 1000000.0 / number_of_points;
+
+                    std::cout << "Frame: " << frame_start_time << " | "
+                              << point_increment << std::endl;
+
+                    current_point = 0;
+
+                    break;
+
+                case 'd':
+                    int16_t eeg_data;
+                    input_stream >> eeg_data;
+
                     array->lock();
-                    double current = 0;
-                    for(int i = 0;i<frame_index;i++){
-                        array->array1.append(frame[i]);
-                        current = dt.count() + avrg_frame_time * i;
-                        if(current<last){
-                            //std::cout<<last<<' '<<current<<std::endl;
-                        }
-                        last = current;
-                        array->array2.append(dt.count() + avrg_frame_time * i);
-                    }
+                    array->array1.append(eeg_data);
+                    array->array2.append(frame_start_time +
+                                         point_increment * current_point++);
                     array->unlock();
-                    
-                    frame_index = 0;
-                    frame_dur = 0;
-                }
+
+                    break;
+
+                case 'a': // Attention
+                case 'h': // Heart Rate
+                case 'm': // Meditation
+                case 'w': // Waves
+                    break;
+
+                case 'r':
+                    int error_code;
+                    input_stream >> error_code;
+
+                    std::cout << "Error with code: " << error_code
+                              << " (" << getStringForError(error_code) << ")"
+                              << std::endl;
+
+                    break;
+
+                default:
+                    std::cout << "Unknown command type: " << type << std::endl;
+                    break;
             }
         }
     }
