79f1e28577febf80f59b8c7f3bbcad430c970ae8
kasumi <kasumi@kasumi-owari.moe>
2018-12-06 15:32:59 +0100

    Added batch specifier each second for openeeg.

--- src/openeeg/main/main.c
+++ src/openeeg/main/main.c
@@ -3,6 +3,7 @@
 #include "esp_event.h"
 #include "esp_event_loop.h"
 #include "nvs_flash.h"
+#include "esp_timer.h"
 #include "driver/adc.h"
 
 esp_err_t event_handler(void *ctx, system_event_t *event)
@@ -18,6 +19,7 @@ void app_main(void)
     adc1_config_width(ADC_WIDTH_BIT_12);
     adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11);
 
+    uint64_t last_specifier = esp_timer_get_time();
     printf("b:0\n");
 
     while (true) {
@@ -25,6 +27,11 @@ void app_main(void)
         int val = adc1_get_raw(ADC1_CHANNEL_0);
 
         printf("d:%i\n", val);
+
+        if (esp_timer_get_time() - last_specifier > 1000000) {
+            last_specifier = esp_timer_get_time();
+            printf("b:0\n");
+        }
 
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
