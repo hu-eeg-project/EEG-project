4ab917fca65fdc9afad5bafeea96d2c0541488e4
kasumi <kasumi@kasumi-owari.moe>
2018-12-18 14:38:49 +0100

    Added metadata support to plotter.

--- src/plotter.py
+++ src/plotter.py
@@ -23,10 +23,12 @@ def main():
 
     y = 2
     x = ceil(len(plots)/y)
-    fig, ax = plt.subplots(x, y, sharex=True, sharey=True)
+    fig, ax = plt.subplots(x, y, sharex=True)
 
     xbuf = 0
     ybuf = 0
+
+    plot_count = 0
 
     for plot in plots:
         f = open(plot, "r")
@@ -39,18 +41,27 @@ def main():
         offset = -1
         count = 0
 
+        line_color = 'green'
+
         for line in lines:
             split_line = line.split(',')
+            if (split_line[0] == "metadata"):
+                if (split_line[1] == 'square'):
+                    line_color = 'red'
+                elif (split_line[1] == 'circle'):
+                    line_color = 'blue'
+                continue
             if offset < 0:
                 offset = float(split_line[0])
             time_array.append(float(split_line[0]) - offset)
             data_array.append(float(split_line[1]))
             if float(split_line[1]) > 3800 or float(split_line[1]) < 200:
-                count += 1
-                #pass
+                #count += 1
+                pass
 
         if count < len(lines) / 3:
-            ax[xbuf, ybuf].plot(time_array, data_array)
+            ax[xbuf, ybuf].plot(time_array, data_array, color=line_color)
+            plot_count += 1
             xbuf += 1
             if xbuf >= x:
                 xbuf = 0
